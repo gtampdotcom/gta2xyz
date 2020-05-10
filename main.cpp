115b51e4c9d0cf83f27f115fb97279bef349fc81
Sektor <gtamulti@gmail.com>
2020-05-10 15:12:09 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
-//GTA2 player speed hack v0.01
+//gta2xyz by Sektor GTAMP.com gtamulti@gmail.com 2016
+//This program fetches GTA2 player coordinates and writes them to the Epic GTA2 Map Editor camera
 //Do whatever you want with this code.
 
 #include <stdio.h>
@@ -6,26 +7,17 @@
 #include <windows.h>
 #include <psapi.h>
 #include <tlhelp32.h>
-//#include <math.h>
+#include <math.h>
 
 using namespace std;
 
-class Ped {
-public:
-  int addr, x, y, z, a;
-  //void set_values (int,int,int,int,int);
-  //void set_values (int addr, int x, int y,int z, int a);
-};
-				
 LPVOID framecount_ptr = (void*)0x5E8108;
 
 int oldframecount;
 int object_number;
 int p1_addr;
 int ped_addr;
-int pedx,pedy,pedz,pedcount;
-int pedcar_x_addr;
-int pedcar_y_addr;
+int pedx,pedy,pedz;
 
 float fx,fy,fz,fa;
 
@@ -43,7 +35,6 @@ int cameraa_addr = 0x5542A4;
 
 HANDLE findProcess(string processname);
 
-/*
 float constrainAngle(float x)
 {	
     x = fmod(x,360);
@@ -51,7 +42,6 @@ float constrainAngle(float x)
         x += 360;
     return x;
 }
-*/
 
 int main( int argc, char* args[] )
 {
@@ -59,32 +49,41 @@ int main( int argc, char* args[] )
 	int playercount;
 	bool quit = false;
 
-	cout << "GTA2 Player Speed Hack v0.02 by Sektor GTAMP.com\n";
-	cout << "------------------------------------------------\n";
+	cout << "GTA2 XYZ v0.1 by Sektor GTAMP.com\n";
+	cout << "-----------------------------------\n";
 		
 	HANDLE hGTA2 = NULL;
-
+	HANDLE hEditor = NULL;
+	
+	hEditor = findProcess("MapEditor.exe");
 	hGTA2 = findProcess("gta2.exe");
 	
-	if(hGTA2 == NULL)
+	if(hEditor == NULL || hGTA2 == NULL)
 	{
-		cout << "Can't find gta2.exe process\n";
+		cout << "Can't find gta2.exe or MapEditor.exe process\n";
 		quit = true;
 	}
 	
 	while( !quit )
 	{
-		Sleep(100);
+		Sleep(5);
 		
 		int framecount,framecount_addr;
 		ReadProcessMemory(hGTA2, framecount_ptr, &framecount_addr, 4, NULL);
 		ReadProcessMemory(hGTA2, (LPVOID)framecount_addr, &framecount, 4, NULL);
-		
-		Ped p[100];
 			
 		if(framecount!=oldframecount)
 		{
 			oldframecount = framecount;
+			
+			if (hEditor != NULL)
+			{
+				fy=256-fy; // the map editor y coordinate is the opposite to GTA2, x is the same
+				WriteProcessMemory(hEditor, (LPVOID)camerax_addr, &fx, 4, NULL);
+				WriteProcessMemory(hEditor, (LPVOID)cameray_addr, &fy, 4, NULL);
+				WriteProcessMemory(hEditor, (LPVOID)cameraa_addr, &fa, 4, NULL);
+				WriteProcessMemory(hEditor, (LPVOID)cameraz_addr, &fz, 4, NULL);
+			}
 			
 			if (hGTA2 != NULL)
 			{
@@ -110,104 +109,40 @@ int main( int argc, char* args[] )
 				
 				object_number=0;
 				
-				playercount=10;
-				
 				if(playercount>6 or playercount<1)
 				{
 					playercount=1;
 				}
 				
-				for(int i = 0; i <1; i++)
+				for(int i = 0; i <playercount; i++)
 				{	
-					pedcount=i;
 					p1struct=p1_addr;	
 	      			p1struct+=object_number;
 	      			int pedstruct;
 	      			pedstruct=p1struct;
-	      			int ped_occupation_addr=0;
-	      			int ped_remap_addr=0;
-	      			ped_occupation_addr=p1struct+0x240;
-	      			ped_remap_addr=p1struct+0x244;
+	      			//int ped_occupation_addr=0;
+	      			//int ped_remap_addr=0;
+	      			//ped_occupation_addr=p1struct+0x240;
+	      			//ped_remap_addr=p1struct+0x244;
 	      			ped_sprite_ptr=p1struct+360;
 					
-					
-					//max run speed = p1struct + 496 or maybe 368
 					//int pedremap_addr=pedstruct+0x244;
-					int pedremap=0;  
-					//WriteProcessMemory(hGTA2, (LPVOID)ped_remap_addr,&pedremap, 1, NULL);
-					int ped_id_addr=p1struct+512;
-					int pedid;
-					ReadProcessMemory(hGTA2, (LPVOID)ped_id_addr,&pedid, 4, NULL);
-					//cout << "ID: " << pedid << "\n"; 
-			
+					//int pedremap=0;  
+					//ReadProcessMemory(hGTA2, (LPVOID)pedremap_addr,&pedremap, 2, NULL);
+				
 					p1struct+=0x1AC;
-	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&p[pedcount].x, 4, NULL);
-	      			
-					//if(i>0)
-					
-					//x=1916928 y=2187264 z=32768 a=1080
-					//p[0].x = 1916928;
-					
-					//WriteProcessMemory(hGTA2, (LPVOID)p1struct,&p[0].x, 4, NULL);
-	      			
-					  
-					p1struct+=0x4;
-	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&p[pedcount].y, 4, NULL);
-	      			//if(i>0)
-	      			//WriteProcessMemory(hGTA2, (LPVOID)p1struct,&p[0].y, 4, NULL);
-	      			//p[0].y=2187264;
-	      			
+	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedx, 4, NULL);
 	      			p1struct+=0x4;
-	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&p[pedcount].z, 4, NULL);
-	      			//if(i>0)
+	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedy, 4, NULL);
+	      			p1struct+=0x4;
+	      			ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedz, 4, NULL);
 	      			
-	      			//p[0].z=65536;
-					//  WriteProcessMemory(hGTA2, (LPVOID)p1struct,&p[0].z, 4, NULL);
-	      			
-	      			//cout << p[0].z << "\n";
-	      			
-	      			
-	      			//p1struct+=0x1AC;
-	      			//ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedx, 4, NULL);
-	      			//p1struct+=0x4;
-	      			//ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedy, 4, NULL);
-	      			//p1struct+=0x4;
-	      			//ReadProcessMemory(hGTA2, (LPVOID)p1struct,&pedz, 4, NULL);
-	      			
-	      			int occupation=0;
+	      			//int occupation=0;
 	      			//int remap=0;	      			
-					int occ=0;
-	      			ReadProcessMemory(hGTA2, (LPVOID)ped_occupation_addr,&occupation, 2, NULL);
-	      			
-	      			//if(i>0)
-					//  WriteProcessMemory(hGTA2, (LPVOID)ped_occupation_addr,&occ, 2, NULL);
+					
+	      			//ReadProcessMemory(hGTA2, (LPVOID)ped_occupation_addr,&occupation, 2, NULL);
 	      			//ReadProcessMemory(hGTA2, (LPVOID)ped_remap_addr,&remap, 2, NULL);
-	      			
 					ReadProcessMemory(hGTA2, (LPVOID)ped_sprite_ptr,&ped_sprite_addr, 4, NULL);
-				
-					//cout << "sprite addy: " << ped_sprite_addr << " \n";
-					
-					
-					
-					int ped_anim=0;
-					int ped_anim_addr=0;
-					
-					ped_anim_addr=ped_sprite_addr+108;
-					ReadProcessMemory(hGTA2, (LPVOID)ped_anim_addr,&ped_anim, 1, NULL);
-					//if(ped_anim==20){
-					//	ped_anim=20;
-					//	WriteProcessMemory(hGTA2, (LPVOID)ped_anim_addr,&ped_anim, 1, NULL);
-				
-						//cout << "ped anim: " << ped_anim << " \n";	
-					//}
-					//ped_anim=3;
-					
-					
-					
-					//11 falling
-					//20 drowning?
-					
-					
 					ped_sprite_addr+=128;
 					ReadProcessMemory(hGTA2, (LPVOID)ped_sprite_addr,&ped_angle_addr, 4, NULL);
 					
@@ -223,80 +158,43 @@ int main( int argc, char* args[] )
 						int pedcar_sprite_addr=pedcar+88;
 						ReadProcessMemory(hGTA2, (LPVOID)pedcar_sprite_addr,&pedcar_sprite, 4, NULL);
 						int pedcar_angle_addr=pedcar_sprite+88;
-						
-						pedcar_x_addr=pedcar_sprite+48;
-						int carx;
-						int cary;
-						ReadProcessMemory(hGTA2, (LPVOID)pedcar_x_addr,&carx, 4, NULL);
-					
-						
-						//781205 car y: 3013908
-						pedcar_y_addr=pedcar_sprite+52;
-						ReadProcessMemory(hGTA2, (LPVOID)pedcar_y_addr,&cary, 4, NULL);
-					
-						//cout << "pedcar: " << pedcar << " car x: " << carx << " car y: " << cary << "\n";
 						int pedcar_angle;
-						ReadProcessMemory(hGTA2, (LPVOID)pedcar_angle_addr,&p[pedcount].a, 2, NULL);
-						//if(i>0)
-						//WriteProcessMemory(hGTA2, (LPVOID)ped_angle_addr,&p[0].a, 2, NULL);
+						ReadProcessMemory(hGTA2, (LPVOID)pedcar_angle_addr,&angle[i], 2, NULL);
 						angle[i] = angle[i]/4;
 						angle[i] = 360-angle[i];
 					}
-					else //on foot
+					else
 					{
-						int ped_walk_addr=ped_sprite_addr+57;
-						int ped_walk=12;
-						int multiplayer_addr = 0x673E2C;
-						int multiplayer;
-						ReadProcessMemory(hGTA2, (LPVOID)multiplayer_addr,&multiplayer, 4, NULL);
-						if(!multiplayer){
-							WriteProcessMemory(hGTA2, (LPVOID)ped_walk_addr,&ped_walk, 4, NULL);
-							//cout << "speed: " << ped_walk << "\n";
-						}
-						int carx;
-						int cary;
-						//carx=781205;
-						
-						//WriteProcessMemory(hGTA2, (LPVOID)pedcar_x_addr,&carx, 4, NULL);
-						//cary=3013908;
-						//WriteProcessMemory(hGTA2, (LPVOID)pedcar_y_addr,&cary, 4, NULL);
-						
-						//ReadProcessMemory(hGTA2, (LPVOID)ped_angle_addr,&angle[i], 2, NULL);
-						ReadProcessMemory(hGTA2, (LPVOID)ped_angle_addr,&p[pedcount].a, 2, NULL);
-						//if(i>0)
-						//p[0].a=0;
-						//WriteProcessMemory(hGTA2, (LPVOID)ped_angle_addr,&p[0].a, 2, NULL);
-						//angle[i] = angle[i]/4;
-						//angle[i] = 360-angle[i];
+						ReadProcessMemory(hGTA2, (LPVOID)ped_angle_addr,&angle[i], 2, NULL);
+						angle[i] = angle[i]/4;
+						angle[i] = 360-angle[i];
 					}	
-					
-					//cout << "i:" << pedcount << "\n";
-					//cout << pedcount << " x=" << p[pedcount].x << " y=" << p[pedcount].y << " z=" << p[pedcount].z << " a=" << p[pedcount].a << "\n";	
-					//cout << pedcount << " x=" << p[0].x << " y=" << p[0].y << " z=" << p[0].z << " a=" << p[0].a << " 0 \n";
-					
-					
 				}
 				
-				/*
+				
 				fx = (float) pedx/16384.0;
 				fy = (float) pedy/16384.0;
 				fz = (float) pedz/16384.0;
 				fa = (float) (angle[0]+180);
 				fa = (float) (constrainAngle(fa));
 				
-				fx-=0.5;
-				fy+=0.5;
-				*/
+				//float fxoffset, fyoffset;
 				
+				//fxoffset = (float) 0.1*sin(M_PI*fa/180.0);
+                //fyoffset = (float) -0.1*cos(M_PI*fa/180.0);
 				
+				//cout << "x offset: " << fxoffset << "\n";
+				//cout << "y offset: " << fyoffset << "\n";
+				cout << framecount << " x=" << fx << " y=" << fy << " z=" << fz << " a=" << fa << "\n";
 				
-				//cout << framecount << " x=" << fx << " y=" << fy << " z=" << fz << " a=" << fa << "\n";		
+				//fx+=fxoffset;
+				//fy+=fyoffset;
+				
 			}
 		}
-		
-		
 	}
 	
+	CloseHandle(hEditor);
 	CloseHandle(hGTA2);
 	
 	return 0;
@@ -322,6 +220,13 @@ HANDLE findProcess(string processname)
 			if(processname=="gta2.exe")
 			{
 				GetModuleFileNameEx(hProcess, NULL, gta2exe, MAX_PATH);
+				//return hProcess;
+			}
+			
+			if(processname=="MapEditor.exe")
+			{
+				GetModuleFileNameEx(hProcess, NULL, editorexe, MAX_PATH);
+				
 			}
 		
   			break;	// break the loop
